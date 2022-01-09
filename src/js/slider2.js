1b952d5813cf9168fedf350c7c7f8662b0d4104a
atiiiye <aty.sokhansanj@gmail.com>
2022-01-09 16:29:49 +0330

    1400/10/19

--- src/js/slider2.js
+++ src/js/slider2.js
@@ -1,35 +1,41 @@
 $(document).ready(function () {
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView:1,
+  var swiper1 = new Swiper(".mySwiper1", {
+    slidesPerView: 1,
     spaceBetween: 10,
-    centeredSlides: true,
+    centeredSlides: false,
     loop: true,
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      1200: {
+        slidesPerView: 5,
+        centeredSlides: true,
       },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+      768: {
+        slidesPerView: 3,
+        centeredSlides: true,
       },
-      breakpoints: {
-       576: {
-          slidesPerView:3,
-        },
-        991: {
-          slidesPerView: 5,
-        },
+      576: {
+        slidesPerView: 2,
+        centeredSlides: false,
       },
-    //   autoplay: {
-    //     delay:3000,
-    //     disableOnInteraction: false,
-    //   },
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
   });
 
   var swiper2 = new Swiper(".mySwiper2", {
     slidesPerView: 1,
     grid: {
-        rows: 2,
+      rows: 2,
     },
     spaceBetween: 30,
     centeredSlides: false,
@@ -38,33 +44,96 @@ $(document).ready(function () {
       clickable: true,
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      1200: {
+        slidesPerView: 4,
+        grid: {
+          rows: 2,
+        },
       },
-      breakpoints: {
-        991:
-        {
-            slidesPerView: 3,
-            grid: {
-                rows: 2,
-            },
+      991: {
+        slidesPerView: 3,
+        grid: {
+          rows: 2,
         },
-        768:
-        {
-            slidesPerView: 2,
-            grid: {
-                rows: 2,
-            },
-            spaceBetween: 30,
+      },
+      576: {
+        slidesPerView: 2,
+        grid: {
+          rows: 2,
         },
-        300:
-        {
-            slidesPerView: 1,
-            grid: {
-                rows: 1,
-            },
-            spaceBetween: 0,
-        }
-    }
+      },
+      300: {
+        slidesPerView: 1,
+        grid: {
+          rows: 1,
+        },
+      },
+    },
+  });
+
+  var swiper3 = new Swiper(".mySwiper3", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    centeredSlides: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      1200: {
+        slidesPerView: 4,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+      576: {
+        slidesPerView: 2,
+      },
+      300: {
+        slidesPerView: 1,
+      },
+    },
+  });
+
+  var swiper4 = new Swiper(".mySwiper4", {
+    slidesPerView: 1,
+    centeredSlides: false,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      1300: {
+        slidesPerView: 5,
+        centeredSlides: true,
+        spaceBetween: 0,
+      },
+      768: {
+        slidesPerView: 3,
+        centeredSlides: true,
+        spaceBetween: 0,
+      },
+      576: {
+        slidesPerView: 2,
+      },
+    },
+    // autoplay: {
+    //   delay:3000,
+    //   disableOnInteraction: false,
+    // },
   });
 });
